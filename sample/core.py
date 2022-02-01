1b25a256101076788e5e7eb3723c8680a7f5d7b6
Wang CHEN <56090906+WangCHEN9@users.noreply.github.com>
2022-02-01 10:50:43 +0100

    add logger

--- sample/core.py
+++ sample/core.py
@@ -1,4 +1,8 @@
 from sample.get_solid import GetSolid
+from loguru import logger
+
+logger.add("file_{time}.log", rotation="00:00")
+logger.debug("That's it, beautiful and simple logging!")
 
 class hi:
     """hiiiiii
